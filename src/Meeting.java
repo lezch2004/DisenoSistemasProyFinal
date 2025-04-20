66106c8e05b649bbc7f2aa44926cbe8902b3bb4f
Luis Eduardo Zaldumbide <lezch2004@gmail.com>
2025-04-19 22:11:22 -0500

    sets and gets

--- src/Meeting.java
+++ src/Meeting.java
@@ -1,5 +1,79 @@
+import java.time.LocalDate;
+import java.util.List;
+
 public class Meeting {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Woeeterld!");
+
+    private String organizer; 
+    private List<String> participants; 
+    private String topic; 
+    private String place; 
+    private LocalDate startDate; 
+    private LocalDate endDate; 
+
+    public Meeting (String organizer, List<String> participants, String topic, String place, LocalDate startDate, LocalDate endDate){
+        setOrganizer(organizer);
+        setParticipants(participants);
+        setTopic(topic);
+        setPlace(place);
+        setStartDate(startDate);
+        setEndDate(endDate);
     }
+
+    public void setOrganizer(String organizer){
+        this.organizer = organizer; 
+    }
+
+    public String getOrganizer(){
+        return organizer;
+    }
+
+    public void setParticipants(List<String> participants){
+        this.participants = participants; 
+    }
+
+    public List<String> getParticipants(){
+        return participants;
+    }
+
+    public void setTopic(String topic){
+        this.topic = topic; 
+    }
+
+    public String getTopic(){
+        return topic;
+    }
+
+    public void setPlace(String place){
+        this.place = place; 
+    }
+
+    public String getPlace(){
+        return place;
+    }
+
+    public void setStartDate(LocalDate startDate){
+        this.startDate = startDate; 
+    }
+
+    public LocalDate getStartDate(){
+        return startDate;
+    }
+
+    public void setEndDate(LocalDate endDate){
+        this.endDate = endDate; 
+    }
+
+    public LocalDate getEndDate(){
+        return endDate;
+    }
+
+
+
+
+
+
 }
+
+
+
+
